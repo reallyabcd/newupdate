a7771468a6a54aa7531dc1628952350ad31bc8cd
reallyabcd <130234216+reallyabcd@users.noreply.github.com>
2023-11-21 16:57:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -66,6 +66,7 @@ from streamlit_folium import folium_static
 #importlib.import_module('FINAL')
 
 st.image('./data/Cover-Img.png')
+
 st.header('RAJA BALWANT SINGH MANAGEMENT & TECHNICAL CAMPUS | MCA Ist 2023 ENTRY PROJECT.')
 st.title('असतो मा सद्गमय । तमसो मा ज्योतिर्गमय । मृत्योर्मा अमृतं गमय ॥')
 st.title('Lead me from the unreal to the real, from darkness to light, and from death to immortality.')
